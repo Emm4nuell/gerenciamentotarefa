3a7e695731efd196d0c0528570de87174b79bd37
Emm4nuell <eduardo.eesf@gmail.om>
2023-11-05 12:23:58 -0300

    Criado o controller USUARIO E TAREFA

---
+++ src/main/java/br/com/gerenciamentotarefa/controller/UsuarioController.java
@@ -0,0 +1,13 @@
+package br.com.gerenciamentotarefa.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/usuario")
+public class UsuarioController {
+
+}
